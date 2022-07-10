fbd1a63f1b7ba7e58aa2d7f10f9f8c347d828fd4
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-07-10 17:34:18 +0100

    Added queries for single resources

--- api/qraphql/schema.js
+++ api/qraphql/schema.js
@@ -1,10 +1,17 @@
-const { GraphQLObjectType, GraphQLString, GraphQLList} = require("graphql")
+const { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLID} = require("graphql")
 const { projects, clients, ProjectType,  ClientType} = require("./../../data-store");
 
 const RootQueryType = new GraphQLObjectType({
     name: "Root",
     description: "Root Query",
     fields: ()=> ({
+        project: {
+            type: ProjectType,
+            description: "Returns A Single Project",
+            args: {id:{type: GraphQLID}},
+            resolve: (_,args)=> projects.find( proj => proj.id == args.id )
+            
+        },
         projects: {
             type: GraphQLList(ProjectType),
             description: "List of All Projects",
@@ -15,7 +22,14 @@ const RootQueryType = new GraphQLObjectType({
             type: GraphQLList(ClientType),
             description: "List of All clients",
             resolve: () => clients
-        }
+        },
+        client: {
+            type: ClientType,
+            description: "Returns A Single Client",
+            args: {id:{type: GraphQLID}},
+            resolve: (_,args)=> clients.find( client => client.id == args.id )
+            
+        },
     })
 });
 
