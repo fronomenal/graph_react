f8955700e1c33bbbf185aace508812d492f858db
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-09-16 22:48:51 +0100

    configured apollo client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/graphql",
+  cache: new InMemoryCache()
+});
 
 function App() {
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <Header/>
-    </>
+    </ApolloProvider>
   )
 }
 
