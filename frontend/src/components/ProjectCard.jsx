412f192e4eb440fdf978e4e472c11ac4bbb83793
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-12-12 01:57:23 +0000

    change anchor to react router link

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function ProjectCard({ project }) {
   return (
     <div className='col-md-6'>
@@ -5,7 +7,7 @@ export default function ProjectCard({ project }) {
         <div className='card-body'>
           <div className='d-flex justify-content-between align-items-center'>
             <h5 className='card-title'>{project.name}</h5> 
-            <a className='btn btn-light' href={`/project/${project.id}`}>  View </a>
+            <Link className='btn btn-light' to={`/project/${project.id}`}>  View </Link>
           </div>
           <p className='small'> Status: <strong>{project.status}</strong> </p>
         </div>
